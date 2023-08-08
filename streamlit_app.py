64abdb16fb8c42159011f39f7c7399fe80e313d8
PakDanan <danan_p@yahoo.com>
2023-08-08 11:21:32 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,47 @@
-#import pandas as pd
-#import numpy as np
+import openai
 import streamlit as st
-st.header('This is a header')
-st.text('This is Text')
+
+st.title("ChatGPT-like clone")
+
+openai.api_key = st.secrets["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"]
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+						 
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+												 
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+				   
+if prompt := st.chat_input("What is up?"):
+									  
+    st.session_state.messages.append({"role": "user", "content": prompt})
+													
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+														  
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+										
+							
+													  
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+											
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
