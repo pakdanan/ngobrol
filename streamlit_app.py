210b22be243fbd8bc529fbefe4fd6195f90a2ee3
Danan Purwantoro <danan.purwantoro@kompas.com>
2023-08-08 10:47:11 +0700

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,12 @@
-
+import pandas as pd
+import numpy as np
+import streamlit as st
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+area = st.checkbox('Area Chart')
+line = st.checkbox('Line Chart')
+if area:
+    st.area_chart(chart_data)
+if line:
+    st.line_chart(chart_data)
