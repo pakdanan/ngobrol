270cbd2cc937fd62cd5045775eeb8f68344433ed
PakDanan <danan_p@yahoo.com>
2023-08-08 11:36:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,43 +5,14 @@ st.title("ChatGPT-like clone")
 
 openai.api_key = st.sidebar.text_input("API-KEY", type="password") #st.secrets["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"]
 
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
+prompt = "Please generate a blog outline on how a beginner can break into the field of data science."
 
-						 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant with extensive experience in data science and technical writing."},
+    {"role": "user", "content": prompt}
+  ]
+)
 
-												 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
-				   
-if prompt := st.chat_input("What is up?"):
-									  
-    st.session_state.messages.append({"role": "user", "content": prompt})
-													
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-														  
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
-        for response in openai.ChatCompletion.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        ):
-            full_response += response.choices[0].delta.get("content", "")
-										
-							
-													  
-            message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
-											
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+print(completion.choices[0].message)
