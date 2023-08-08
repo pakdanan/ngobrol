1f6defcbfb0c919146136b9d3c218d37139f76bd
PakDanan <danan_p@yahoo.com>
2023-08-08 10:57:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,5 @@
-import pandas as pd
-import numpy as np
+#import pandas as pd
+#import numpy as np
 import streamlit as st
-chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=['a', 'b', 'c'])
-area = st.checkbox('Area Chart')
-line = st.checkbox('Line Chart')
-if area:
-    st.area_chart(chart_data)
-if line:
-    st.line_chart(chart_data)
+st.header('This is a header')
+st.text('This is Text')
